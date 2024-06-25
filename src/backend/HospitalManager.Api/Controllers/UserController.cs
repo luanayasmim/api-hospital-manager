d54f68f942ca53ded623dbcbe803557fd878239d
Luana Yasmim <luanayasmim11@outlook.com>
2024-06-25 18:06:42 -0300

    feat: added userController

---
+++ src/backend/HospitalManager.Api/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using HospitalManager.Application.UseCases.User.Register;
+using HospitalManager.Communication.Requests.User;
+using HospitalManager.Communication.Responses.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalManager.Api.Controllers;
+
+[Route("/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
+    public async Task<IActionResult> Register([FromServices] IRegisterUserUseCase useCase, [FromBody] RequestRegisterUserJson request)
+    {
+        var result = await useCase.Execute(request);
+
+        return Created(string.Empty, result);
+    }
+}
