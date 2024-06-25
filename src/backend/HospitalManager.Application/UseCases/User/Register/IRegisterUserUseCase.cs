d6029451f9ffea246a0f8dbaee690cdfd87d4290
Luana Yasmim <luanayasmim11@outlook.com>
2024-06-25 18:04:56 -0300

    feat: added user's use Case

---
+++ src/backend/HospitalManager.Application/UseCases/User/Register/IRegisterUserUseCase.cs
@@ -0,0 +1,8 @@
+﻿using HospitalManager.Communication.Requests.User;
+using HospitalManager.Communication.Responses.User;
+
+namespace HospitalManager.Application.UseCases.User.Register;
+public interface IRegisterUserUseCase
+{
+    public Task<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request);
+}
---
+++ src/backend/HospitalManager.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using HospitalManager.Application.Services.Cryptography;
+using HospitalManager.Communication.Requests.User;
+using HospitalManager.Communication.Responses.User;
+using HospitalManager.Domain.Repositories;
+using HospitalManager.Domain.Repositories.User;
+using HospitalManager.Exceptions;
+using HospitalManager.Exceptions.ExceptionsBase;
+
+namespace HospitalManager.Application.UseCases.User.Register;
+public class RegisterUserUseCase : IRegisterUserUseCase
+{
+    private readonly IUserReadOnlyRepository _readOnlyRepository;
+    private readonly IUserWriteOnlyRepository _writeOnlyRepository;
+    private readonly IUnityOfWork _unityOfWork;
+    private readonly IMapper _mapper;
+    private readonly PasswordEncripter _passwordEncripter;
+
+    public RegisterUserUseCase(
+        IUserReadOnlyRepository readOnlyRepository,
+        IUserWriteOnlyRepository writeOnlyRepository,
+        IUnityOfWork unityOfWork,
+        IMapper mapper,
+        PasswordEncripter passwordEncripter)
+    {
+        _readOnlyRepository = readOnlyRepository;
+        _writeOnlyRepository = writeOnlyRepository;
+        _unityOfWork = unityOfWork;
+        _mapper = mapper;
+        _passwordEncripter = passwordEncripter;
+    }
+
+    public async Task<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request)
+    {
+        await Validate(request);
+
+        var user = _mapper.Map<Domain.Entities.User>(request);
+        user.Password = _passwordEncripter.Encrypt(request.Password);
+
+        await _writeOnlyRepository.Add(user);
+
+        await _unityOfWork.Commit();
+
+        return new ResponseRegisteredUserJson
+        {
+            Id = user.Id
+        };
+    }
+
+    private async Task Validate(RequestRegisterUserJson request)
+    {
+        var validator = new RegisterUserValidator();
+
+        var result = validator.Validate(request);
+
+        var emailExist = await _readOnlyRepository.ExistActiveUserWithEmail(request.Email);
+
+        if (emailExist)
+            result.Errors.Add(new FluentValidation.Results.ValidationFailure(string.Empty, ResourceMessagesExceptions.EMAIL_ALREADY_REGISTERED));
+
+        if (!result.IsValid)
+        {
+            var errorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
+
+            throw new ErrorOnValidationException(errorMessages);
+        }
+    }
+}
