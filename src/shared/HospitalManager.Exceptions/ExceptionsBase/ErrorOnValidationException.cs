4cc199a4ebc9224e5e9b0ac505bddbd15c0351fd
Luana Yasmim <luanayasmim11@outlook.com>
2024-06-25 16:27:23 -0300

    feat: added exceptions project and messages in en and pt-BR

---
+++ src/shared/HospitalManager.Exceptions/ExceptionsBase/ErrorOnValidationException.cs
@@ -0,0 +1,8 @@
+﻿namespace HospitalManager.Exceptions.ExceptionsBase;
+
+public class ErrorOnValidationException : HospitalManagerException
+{
+    public IList<string> ErrorMessages { get; set; }
+
+    public ErrorOnValidationException(IList<string> errorMessages) => ErrorMessages = errorMessages;
+}
---
+++ src/shared/HospitalManager.Exceptions/ExceptionsBase/HospitalManagerException.cs
@@ -0,0 +1,5 @@
+﻿namespace HospitalManager.Exceptions.ExceptionsBase;
+
+public class HospitalManagerException : SystemException
+{
+}
