62f51d6923d621291093c2c5637b649cf633f0e2
Luana Yasmim <luanayasmim11@outlook.com>
2024-06-27 09:28:06 -0300

    feat: configuration for database in memory

--- src/backend/HospitalManager.Infrastructure/Extensions/ConfigurationExtension.cs
+++ src/backend/HospitalManager.Infrastructure/Extensions/ConfigurationExtension.cs
@@ -4,6 +4,11 @@ using Microsoft.Extensions.Configuration;
 namespace HospitalManager.Infrastructure.Extensions;
 public static class ConfigurationExtension
 {
+    public static bool IsUnitTestEnvironment(this IConfiguration configuration)
+    {
+        return configuration.GetValue<bool>("InMemoryTest");
+    }
+
     public static DatabaseType DatabaseType(this IConfiguration configuration)
     {
         var databaseType = configuration.GetConnectionString("DatabaseType");
