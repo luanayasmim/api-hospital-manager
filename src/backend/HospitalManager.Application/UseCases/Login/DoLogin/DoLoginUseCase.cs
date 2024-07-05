2299d5147711dd41add6df0d4aef7cf16431aca0
Luana Yasmim <luanayasmim11@outlook.com>
2024-07-05 11:56:52 -0300

    feat: added a login useCase

---
+++ src/backend/HospitalManager.Application/UseCases/Login/DoLogin/DoLoginUseCase.cs
@@ -0,0 +1,31 @@
+﻿using HospitalManager.Application.Services.Cryptography;
+using HospitalManager.Communication.Requests.Login;
+using HospitalManager.Communication.Responses.User;
+using HospitalManager.Domain.Repositories.User;
+using HospitalManager.Exceptions.ExceptionsBase;
+
+namespace HospitalManager.Application.UseCases.Login.DoLogin;
+public class DoLoginUseCase : IDoLoginUseCase
+{
+    private readonly IUserReadOnlyRepository _readOnlyRepository;
+    private readonly PasswordEncripter _passwordEncripter;
+
+    public DoLoginUseCase(IUserReadOnlyRepository readOnlyRepository, PasswordEncripter passwordEncripter)
+    {
+        _readOnlyRepository = readOnlyRepository;
+        _passwordEncripter = passwordEncripter;
+    }
+
+    public async Task<ResponseRegisteredUserJson> Execute(RequestLoginJson request)
+    {
+        var encripteredPassword = _passwordEncripter.Encrypt(request.Password);
+
+        var user = await _readOnlyRepository.GetByEmailAndPassword(request.Email, encripteredPassword) ?? throw new InvalidLoginException();
+
+        return new ResponseRegisteredUserJson
+        {
+            Id = user.Id,
+            Name = user.Name,
+        };
+    }
+}
