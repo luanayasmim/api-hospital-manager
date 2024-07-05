652f736409ae3fb0a6e08c593b9640742b4430ee
Luana Yasmim <luanayasmim11@outlook.com>
2024-07-05 11:53:23 -0300

    feat: added class to request a login

---
+++ src/shared/HospitalManager.Communication/Requests/Login/RequestLoginJson.cs
@@ -0,0 +1,6 @@
+﻿namespace HospitalManager.Communication.Requests.Login;
+public class RequestLoginJson
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
