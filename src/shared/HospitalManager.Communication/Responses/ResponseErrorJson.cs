eeb5a2414d39519cf95dbedbd3cedbf83809c138
Luana Yasmim <luanayasmim11@outlook.com>
2024-07-11 11:43:39 -0300

    feat: added property to check if a token is expired

--- src/shared/HospitalManager.Communication/Responses/ResponseErrorJson.cs
+++ src/shared/HospitalManager.Communication/Responses/ResponseErrorJson.cs
@@ -2,6 +2,7 @@
 public class ResponseErrorJson
 {
     public IList<string> Errors { get; set; }
+    public bool TokenIsExpired { get; set; }
 
     public ResponseErrorJson(IList<string> errors) => Errors = errors;
 
