a4594e58b48c36bb33f5342bacc52810947ddf22
Luana Yasmim <luanayasmim11@outlook.com>
2024-07-11 11:47:10 -0300

    feat: added a authorization functions to a token access

---
+++ src/backend/HospitalManager.Api/Filters/AuthenticatedUserFilter.cs
@@ -0,0 +1,62 @@
+﻿using HospitalManager.Communication.Responses;
+using HospitalManager.Domain.Repositories.User;
+using HospitalManager.Domain.Security.Tokens;
+using HospitalManager.Exceptions.ExceptionsBase;
+using HospitalManager.Exceptions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace HospitalManager.Api.Filters;
+
+public class AuthenticatedUserFilter : IAsyncAuthorizationFilter
+{
+    private readonly IAccessTokenValidator _tokenValidator;
+    private readonly IUserReadOnlyRepository _readOnlyRepository;
+
+    public AuthenticatedUserFilter(IAccessTokenValidator tokenValidator, IUserReadOnlyRepository readOnlyRepository)
+    {
+        _tokenValidator = tokenValidator;
+        _readOnlyRepository = readOnlyRepository;
+    }
+
+    public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+    {
+        try
+        {
+            var token = TokenOnRequest(context);
+            var userIdentifier = _tokenValidator.ValidateAndGetUserIdentifier(token);
+            var exist = await _readOnlyRepository.ExistActiveUserWithIdentifier(userIdentifier);
+
+            if (!exist)
+            {
+                throw new HospitalManagerException(ResourceMessagesExceptions.USER_WITHOUT_PERMISSION_ACCESS_RESOURCE);
+            }
+        }
+        catch (SecurityTokenExpiredException ex)
+        {
+            context.Result = new UnauthorizedObjectResult(new ResponseErrorJson(ex.Message));
+        }
+        catch (HospitalManagerException)
+        {
+            context.Result = new UnauthorizedObjectResult(new ResponseErrorJson("TokenIsExpired") { TokenIsExpired = true });
+        }
+        catch
+        {
+            context.Result = new UnauthorizedObjectResult(new ResponseErrorJson(ResourceMessagesExceptions.USER_WITHOUT_PERMISSION_ACCESS_RESOURCE));
+
+        }
+    }
+
+    private static string TokenOnRequest(AuthorizationFilterContext context)
+    {
+        var authentication = context.HttpContext.Request.Headers.Authorization.ToString();
+
+        if (string.IsNullOrEmpty(authentication))
+        {
+            throw new HospitalManagerException(ResourceMessagesExceptions.NO_TOKEN);
+        }
+
+        return authentication["Bearer ".Length..].Trim();
+    }
+}
--- src/backend/HospitalManager.Api/Program.cs
+++ src/backend/HospitalManager.Api/Program.cs
@@ -1,16 +1,49 @@
 using HospitalManager.Api.Converters;
 using HospitalManager.Api.Filters;
 using HospitalManager.Api.Middlewares;
+using HospitalManager.Api.Token;
 using HospitalManager.Application;
+using HospitalManager.Domain.Security.Tokens;
 using HospitalManager.Infrastructure;
 using HospitalManager.Infrastructure.Extensions;
 using HospitalManager.Infrastructure.Migrations;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new StringConverter()));
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Baerer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Baerer scheme.
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      Example: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 builder.Services.AddMvc(options=>options.Filters.Add(typeof(ExceptionFilter)));
 
@@ -18,7 +51,11 @@ builder.Services.AddMvc(options=>options.Filters.Add(typeof(ExceptionFilter)));
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication(builder.Configuration);
 
+builder.Services.AddScoped<ITokenProvider, HttpContextTokenValue>();
+
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
