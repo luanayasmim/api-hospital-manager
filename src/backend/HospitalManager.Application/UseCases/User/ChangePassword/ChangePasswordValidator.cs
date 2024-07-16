de726b8bdd38f59153bac6b0f80de5744f4d28f6
Luana Yasmim <luanayasmim11@outlook.com>
2024-07-16 17:08:45 -0300

    feat: created a changePasswordValidator

---
+++ src/backend/HospitalManager.Application/UseCases/User/ChangePassword/ChangePasswordValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using HospitalManager.Application.SharedValidators;
+using HospitalManager.Communication.Requests.User;
+
+namespace HospitalManager.Application.UseCases.User.ChangePassword;
+public class ChangePasswordValidator : AbstractValidator<RequestChangePasswordJson>
+{
+    public ChangePasswordValidator()
+    {
+        RuleFor(user => user.NewPassword)
+            .SetValidator(new PasswordValidator<RequestChangePasswordJson>());
+    }
+}
