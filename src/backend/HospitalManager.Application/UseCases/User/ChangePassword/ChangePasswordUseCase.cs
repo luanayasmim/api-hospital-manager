11a763c85a5a7c60428a4e3b2b46d3a7f1f81b90
Luana Yasmim <luanayasmim11@outlook.com>
2024-07-16 17:09:18 -0300

    feat: created changePassowordUseCase implemetation

---
+++ src/backend/HospitalManager.Application/UseCases/User/ChangePassword/ChangePasswordUseCase.cs
@@ -0,0 +1,58 @@
+﻿using FluentValidation.Results;
+using HospitalManager.Communication.Requests.User;
+using HospitalManager.Domain.Repositories;
+using HospitalManager.Domain.Repositories.User;
+using HospitalManager.Domain.Security.Cryptography;
+using HospitalManager.Domain.Services.LoggedUser;
+using HospitalManager.Exceptions;
+using HospitalManager.Exceptions.ExceptionsBase;
+
+namespace HospitalManager.Application.UseCases.User.ChangePassword;
+public class ChangePasswordUseCase : IChangePasswordUseCase
+{
+    private readonly ILoggedUser _loggedUser;
+    private readonly IUserUpdateOnlyRepository _updateOnlyRepository;
+    private readonly IUnityOfWork _unityOfWork;
+    private readonly IPasswordEncripter _passwordEncripter;
+
+    public ChangePasswordUseCase(
+        ILoggedUser loggedUser,
+        IUserUpdateOnlyRepository updateOnlyRepository,
+        IUnityOfWork unityOfWork,
+        IPasswordEncripter passwordEncripter)
+    {
+        _loggedUser = loggedUser;
+        _updateOnlyRepository = updateOnlyRepository;
+        _unityOfWork = unityOfWork;
+        _passwordEncripter = passwordEncripter;
+    }
+
+    public async Task Execute(RequestChangePasswordJson request)
+    {
+        var loggedUser = await _loggedUser.User();
+
+        Validate(request, loggedUser);
+
+        var user = await _updateOnlyRepository.GetById(loggedUser.Id);
+
+        user.Password = _passwordEncripter.Encrypt(request.NewPassword);
+
+        _updateOnlyRepository.Update(user);
+
+        await _unityOfWork.Commit();
+
+    }
+
+    private void Validate(RequestChangePasswordJson request, Domain.Entities.User loggedUser)
+    {
+        var result = new ChangePasswordValidator().Validate(request);
+
+        var currentPasswordEncripted = _passwordEncripter.Encrypt(request.Password);
+
+        if (!currentPasswordEncripted.Equals(loggedUser.Password))
+            result.Errors.Add(new ValidationFailure(string.Empty, ResourceMessagesExceptions.PASSWORD_DIFFERENT_CURRENT_PASSWORD));
+
+        if (!result.IsValid)
+            throw new ErrorOnValidationException(result.Errors.Select(error=>error.ErrorMessage).ToList());
+    }
+}
